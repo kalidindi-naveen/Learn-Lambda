7d78bd0a25870628cc7d9f0a45387c67c995c665
kalidindi-naveen <151353708+kalidindi-naveen@users.noreply.github.com>
2024-04-19 11:40:20 +0530

    hello-python.py

--- hello-python.py
+++ hello-python.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-  message = 'Hello {} !'.format(event['key1'])
+  message = 'Hello From NK !'
   return {
-      'message' : message
+      "message": message
   }
